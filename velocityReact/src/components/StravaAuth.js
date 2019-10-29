3937f9cfdb04d9fa66705f53493ad9de58296f60
James Holton <james_holton@yahoo.co.uk>
2019-10-29 15:44:31 +0000

    Cleaned up components and added link to Strava

--- velocityReact/src/components/StravaAuth.js
+++ velocityReact/src/components/StravaAuth.js
@@ -2,16 +2,12 @@ import React, { Component } from 'react'
 
 class StravaAuth extends Component {
 
-  onStravaClick = () => {
-    alert ("Hello")
-  };
-
   render() {
     return (
       <div>
-        <h2>Strava Authorization</h2>
+        <h2>click here to Authorize us to use your Strava data</h2>
         <br/>
-        <button value="stravaAuth" onClick={this.onStravaClick.bind(this)}>Strava</button>
+        <a href='http://www.strava.com/oauth/authorize?client_id=40250&response_type=code&redirect_uri=http://localhost:3000/&scope=read,activity:read,activity:read_all&approval_prompt=force'> Authorize</a>
       </div>
     )
   }
